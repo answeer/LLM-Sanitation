e7d1d0fb950691fc8923802c90e5b9583230f8b1
李晶 <lijing@lijingdeMacBook-Pro.local>
2024-05-09 16:58:56 +0800

    update

--- llm-guard/input_scanner/input_pii.py
+++ llm-guard/input_scanner/input_pii.py
@@ -5,8 +5,36 @@ vault = Vault()
 from llm_guard.input_scanners import Anonymize
 from llm_guard.input_scanners.anonymize_helpers import BERT_LARGE_NER_CONF
 
+default_entity_types = [
+    "CREDIT_CARD",
+    "CRYPTO",
+    "DATE_TIME",
+    "EMAIL_ADDRESS",
+    "IBAN_CODE",
+    "IP_ADDRESS",
+    "NRP",
+    "LOCATION",
+    "PERSON",
+    "PHONE_NUMBER",
+    "MEDICAL_LICENSE",
+    "URL",
+    "US_SSN",
+    "US_ITIN",
+    "US_BANK_NUMBER",
+    "US_PASSPORT",
+    "US_DRIVER_LICENSE",
+    "CREDIT_CARD_RE",
+    "UUID",
+    "EMAIL_ADDRESS_RE",
+    "US_SSN_RE",
+    "IN_PAN",
+    "IN_AADHAAR",
+    "IN_VEHICLE_REGISTRATION",
+]
+
+
 scanner = Anonymize(vault, preamble="Insert before prompt", allowed_names=["John Doe"], hidden_names=["Test LLC"],
-                    recognizer_conf=BERT_LARGE_NER_CONF, language="en")
+                    recognizer_conf=BERT_LARGE_NER_CONF,threshold=0.5,entity_types=default_entity_types, language="en")
 
 prompt= """
 Hello, my name is David Johnson and I live in Maine.
@@ -19,6 +47,7 @@ My passport: 191280342 and my phone number: (212) 555-1234.
 This is a valid International Bank Account Number: IL150120690000003111111 . Can you please check the status on bank account 954567876544?
 
 Kate's social security number is 078-05-1126.  Her driver license? it is 1234567A.
+PAN ASCDE1234R
 """
 sanitized_prompt, is_valid, risk_score = scanner.scan(prompt)
 
