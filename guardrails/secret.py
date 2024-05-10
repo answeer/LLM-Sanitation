2c5c870db24cd0e47b9a9661d875e9e28c1ea7da
李晶 <lijing@lijingdeMacBook-Pro.local>
2024-05-10 11:40:27 +0800

    update

--- guardrails/secret.py
+++ guardrails/secret.py
@@ -27,10 +27,15 @@ guard = Guard().use(SecretsPresent, on_fail="fix")
 #     print(e)
 
 text = """
-def hello():
-    user_id = "1234"
-    user_pwd = "password1234"
-    user_api_key = "sk-xhdfgtest"
+red_herring = 'DEADBEEF'
+id = 'YW1pYWx3YXlzZ2VuZXJhdGluZ3BheWxvYWRzd2hlbmltaHVuZ3J5b3JhbWlhbHdheXNodW5ncnk'
+
+base64_secret = 'c2VjcmV0IG1lc3NhZ2Ugc28geW91J2xsIG5ldmVyIGd1ZXNzIG15IHBhc3N3b3Jk'
+hex_secret = '8b1118b376c313ed420e5133ba91307817ed52c2'
+basic_auth = 'http://username:whywouldyouusehttpforpasswords@example.com'
+
+aws_access_key = 'AKIAIOSFODNN7EXAMPLE'
+aws_secret_access_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
 """
 # Test with a given text
 output = guard.parse(
