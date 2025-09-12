c6384cecf6b1f32e6c8d213c688755b63ac71d5a
Jing Li <1187048178@qq.com>
2025-09-12 18:14:14 +0800

    Update validjson.py

--- guardrails/validjson.py
+++ guardrails/validjson.py
@@ -1,15 +1,50 @@
-# Import Guard and Validator
-from guardrails.hub import ValidJson
-from guardrails import Guard
+def create_tool_calling_agent(
+    model: LanguageModelLike,
+    tools: Union[ToolNode, Sequence[BaseTool]],
+    system_prompt: Optional[str] = None,
+):
 
-# Setup Guard
-guard = Guard().use(ValidJson, on_fail="exception")
+    tools = get_tools()
+    model = model.bind_tools(tools)
 
-guard.validate('{ "value": "a test value" }')  # Validator passes
+    # Define the function that determines which node to go to
+    def should_continue(state: ChatAgentState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        # If there are function calls, continue. else, end
+        if last_message.get("tool_calls"):
+            return "continue"
+        else:
+            return "end"
 
-try:
-    guard.validate(
-        '{ "value": "a test value", }'
-    )  # Validator fails; note the trailing comma
-except Exception as e:
-    print(e)
+    if system_prompt:
+        preprocessor = RunnableLambda(
+            lambda state: [{"role": "system", "content": system_prompt}]
+            + state["messages"]
+        )
+    else:
+        preprocessor = RunnableLambda(lambda state: state["messages"])
+
+    def call_model(
+        state: ChatAgentState,
+        config: RunnableConfig,
+    ):
+        response = model_runnable.invoke(state, config)
+        return {"messages": [response]}
+
+    model_runnable = preprocessor | model
+    workflow = StateGraph(ChatAgentState)
+    workflow.add_node("agent", RunnableLambda(call_model))
+    workflow.add_node("tools", ChatAgentToolNode(tools))
+    workflow.set_entry_point("agent")
+    workflow.add_conditional_edges(
+        "agent",
+        should_continue,
+        {
+            "continue": "tools",
+            "end": END,
+        },
+    )
+    workflow.add_edge("tools", "agent")
+
+    return workflow.compile()
