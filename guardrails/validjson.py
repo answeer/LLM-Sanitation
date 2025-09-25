f56742132daa2201487006a34a3192625810ba1d
Jing Li <1187048178@qq.com>
2025-09-25 10:26:43 +0800

    Update validjson.py

--- guardrails/validjson.py
+++ guardrails/validjson.py
@@ -1,50 +1,20 @@
-def create_tool_calling_agent(
-    model: LanguageModelLike,
-    tools: Union[ToolNode, Sequence[BaseTool]],
-    system_prompt: Optional[str] = None,
-):
+from openai import OpenAI
+import os
+DATABRICKS_TOKEN = os.environ.get('DATABRICKS_TOKEN')
+client = OpenAI(
+    api_key=DATABRICKS_TOKEN,
+    base_url="https://xxxx/serving-endpoints"
+)
 
-    tools = get_tools()
-    model = model.bind_tools(tools)
-
-    # Define the function that determines which node to go to
-    def should_continue(state: ChatAgentState):
-        messages = state["messages"]
-        last_message = messages[-1]
-        # If there are function calls, continue. else, end
-        if last_message.get("tool_calls"):
-            return "continue"
-        else:
-            return "end"
-
-    if system_prompt:
-        preprocessor = RunnableLambda(
-            lambda state: [{"role": "system", "content": system_prompt}]
-            + state["messages"]
-        )
-    else:
-        preprocessor = RunnableLambda(lambda state: state["messages"])
-
-    def call_model(
-        state: ChatAgentState,
-        config: RunnableConfig,
-    ):
-        response = model_runnable.invoke(state, config)
-        return {"messages": [response]}
-
-    model_runnable = preprocessor | model
-    workflow = StateGraph(ChatAgentState)
-    workflow.add_node("agent", RunnableLambda(call_model))
-    workflow.add_node("tools", ChatAgentToolNode(tools))
-    workflow.set_entry_point("agent")
-    workflow.add_conditional_edges(
-        "agent",
-        should_continue,
+response = client.chat.completions.create(
+    model="databricks-claude-3-7-sonnet",
+    messages=[
         {
-            "continue": "tools",
-            "end": END,
-        },
-    )
-    workflow.add_edge("tools", "agent")
+            "role": "user",
+            "content": "What is an LLM agent?"
+        }
+    ],
+    max_tokens=5000
+)
 
-    return workflow.compile()
+print(response.choices[0].message.content)
